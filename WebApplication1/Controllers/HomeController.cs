4b91779e634a77da43b73343dfdcc563ea8661c5
faragh <mohmmad_kratos47@yahoo.com>
2021-01-01 23:54:04 +0330

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Solid()
+        {
+            return View();
+        }
+        public IActionResult ProgrammingSkills()
+        {
+            return View();
+        }
+        public IActionResult Habits()
+        {
+            return View("/Views/Home/7Habits.cshtml");
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
